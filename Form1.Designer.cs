d97265c263a313f6e8fc5757c188de09fe938181
hassanawan02 <112867584+hassanawan02@users.noreply.github.com>
2024-08-30 22:44:16 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MyFirstActualProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(12, 307);
+            button1.Name = "button1";
+            button1.Size = new Size(713, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Click here";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(292, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Write your weight in kg. Write a float value";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(12, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(713, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(318, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Write your height in meters. Write a float value";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 174);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(713, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
+            ClientSize = new Size(737, 375);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "My App";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+    }
+}
